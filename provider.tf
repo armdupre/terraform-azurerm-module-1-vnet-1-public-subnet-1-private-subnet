789f81a87a4bd537010737dc97eab7fbd8fdadfe
armdupre <a-joseph.dupre@keysight.com>
2023-05-24 19:38:18 -0400

    Updated.

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "azurerm" {
-	subscription_id = var.AzureSubscriptionId
+	subscription_id = var.SubscriptionId
 	features {
 		resource_group {
 			prevent_deletion_if_contains_resources = false
