4db7bc4e4a184cd5f8bb46e95b4db1d9e543fc46
armdupre <a-joseph.dupre@keysight.com>
2023-06-30 10:19:16 -0400

    Updated.

--- output.tf
+++ output.tf
@@ -2,6 +2,7 @@ output "PrivateNetworkSecurityGroup" {
 	description = "Network security group resource associated with the private subnet"
 	value = {
 		"id" : azurerm_network_security_group.PrivateNetworkSecurityGroup.id
+		"name" : azurerm_network_security_group.PrivateNetworkSecurityGroup.name
 	}
 }
 
@@ -16,6 +17,7 @@ output "PublicNetworkSecurityGroup" {
 	description = "Network security group resource associated with the public subnet"
 	value = {
 		"id" : azurerm_network_security_group.PublicNetworkSecurityGroup.id
+		"name" : azurerm_network_security_group.PublicNetworkSecurityGroup.name
 	}
 }
 
